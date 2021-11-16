99a86c91dbffa47527fc7616dde293c026f42834
yavetisyan <y.avetisyan88@gmail.com>
2021-11-17 01:27:24 +0400

    material ui

--- src/components/BeedLists.js
+++ src/components/BeedLists.js
@@ -1,26 +1,46 @@
+import {
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
+//import "./BreedList.css";
+import CircularProgress from "@mui/material/CircularProgress";
 
 class BreedList extends React.Component {
   state = {
     breedList: [],
+    src: "",
+    isLoading: false,
+    isOpen: false,
   };
 
   getBreeds = async () => {
+    this.setState({
+      isLoading: true,
+    });
     const responce = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await responce.json();
     this.setState({
       breedList: data.message,
+      isLoading: false,
     });
   };
 
-  imgSrc = async (breed) => {
-    const response = await fetch(
-      `https://dog.ceo/api/breed/${breed}/images/random`
-    );
-    const data = await response.json();
+  onListItemButtonClick = async (breed) => {
+    //const response = await fetch(
+    //  `https://dog.ceo/api/breed/${breed}/images/random`
+    //);
+    //const data = await response.json();
+    //this.setState({
+    //  src: data.message,
+    //});
+
     this.setState({
-      src: data.message,
+      isOpen: !this.state.isOpen,
     });
   };
 
@@ -37,30 +57,60 @@ class BreedList extends React.Component {
           display: "flex",
         }}
       >
-			<div></div>
-        <div class="loader"></div>
+        {this.state.isLoading ? (
+          <CircularProgress
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "trnaslateX(-50%)",
+              zIndex: 50,
+            }}
+          />
+        ) : (
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          >
+            {breedListArr.map((breed) => {
+              return (
+                <React.Fragment>
+                  <ListItemButton
+                    component="li"
+                    key={breed}
+                    onClick={() => this.onListItemButtonClick(breed)}
+                    style={{
+                      margin: 10,
+                      cursor: "pointer",
+                      color: this.state.breedList[breed].length
+                        ? "red"
+                        : "green",
+                    }}
+                  >
+                    <ListItemText primary={breed} />
+                  </ListItemButton>
+                  {this.state.breedList[breed].length ? (
+                    <Collapse
+                      in={this.state.isOpen}
+                      timeout="auto"
+                      unmountOnExit
+                    >
+                      <List component="div" disablePadding>
+                        <ListItemButton sx={{ pl: 4 }}>
+                          <ListItemText primary="Starred" />
+                        </ListItemButton>
+                      </List>
+                    </Collapse>
+                  ) : null}
+                </React.Fragment>
+              );
+            })}
+          </List>
+        )}
 
-        <ul>
-          {breedListArr.map((breed) => {
-            return (
-              <li
-                key={uuidv4()}
-                onClick={() => this.imgSrc(breed)}
-                style={{
-                  margin: 10,
-                  cursor: "pointer",
-                  color: this.state.breedList[breed].length ? "red" : "green",
-                }}
-              >
-                {breed}
-              </li>
-            );
-          })}
-        </ul>
         {this.state.src === "" ? null : (
           <img
             src={this.state.src}
-            style={{ width: 300, height: 300, marginTop: 30 }}
+            style={{ maxWidth: "100%", height: 500, marginTop: 30 }}
             alt={"Dog Pic"}
           />
         )}
