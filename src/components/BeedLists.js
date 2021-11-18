feee0c092ade8ccbf1394576986c3889573d4082
yavetisyan <y.avetisyan88@gmail.com>
2021-11-18 14:52:38 +0400

    asd

--- src/components/BeedLists.js
+++ src/components/BeedLists.js
@@ -2,8 +2,8 @@ import {
   Collapse,
   List,
   ListItemButton,
-  ListItemIcon,
   ListItemText,
+  TextField,
 } from "@mui/material";
 import React from "react";
 //import { v4 as uuidv4 } from "uuid";
@@ -21,6 +21,7 @@ class BreedList extends React.Component {
     isLoading: false,
     isOpen: false,
     selectedBreedName: "",
+    searchText: "",
   };
 
   getBreeds = async () => {
@@ -63,59 +64,86 @@ class BreedList extends React.Component {
           display: "flex",
         }}
       >
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={this.state.searchText}
+          margin="dense"
+          onChange={(e) => {
+            this.setState({
+              searchText: e.target.value,
+            });
+          }}
+        />
+
         {this.state.isLoading ? (
           <CircularProgress
             sx={{
               position: "absolute",
               top: "50%",
               left: "50%",
-              transform: "trnaslateX(-50%)",
+              transform: "translateX(-50%)",
               zIndex: 50,
             }}
           />
         ) : (
           <List
-            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+            sx={{
+              width: "100%",
+              maxWidth: 360,
+              backgroundColor: "background.paper",
+            }}
           >
-            {breedListArr.map((breed) => {
-              return (
-                <React.Fragment>
-                  <ListItemButton
-                    component="li"
-                    key={breed}
-                    onClick={() => this.onListItemButtonClick(breed)}
-                    style={{
-                      margin: 10,
-                      cursor: "pointer",
-                      color: this.state.breedList[breed].length
-                        ? "red"
-                        : "green",
-                    }}
-                  >
-                    <ListItemText primary={breed} />
-                  </ListItemButton>
-                  {this.state.breedList[breed].length ? (
-                    <Collapse
-                      in={this.state.isOpen}
-                      timeout="auto"
-                      unmountOnExit
+            {breedListArr
+              .filter((breed) => breed.includes(this.state.searchText))
+              .map((breed) => {
+                return (
+                  <React.Fragment>
+                    <ListItemButton
+                      component="li"
+                      key={breed}
+                      onClick={() => this.onListItemButtonClick(breed)}
+                      style={{
+                        margin: 10,
+                        cursor: "pointer",
+                        color: this.state.breedList[breed].length
+                          ? "red"
+                          : "green",
+                      }}
                     >
-                      <List component="div" disablePadding>
-                        <ListItemButton sx={{ pl: 4 }}>
-                          <ListItemText primary="Starred" />
-                        </ListItemButton>
-                      </List>
-                    </Collapse>
-                  ) : null}
-                </React.Fragment>
-              );
-            })}
+                      <ListItemText primary={breed} />
+                    </ListItemButton>
+                    {this.state.breedList[breed].length ? (
+                      <Collapse
+                        in={this.state.isOpen}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List component="div" disablePadding>
+                          <ListItemButton sx={{ pl: 4 }}>
+                            <ListItemText primary="Starred" />
+                          </ListItemButton>
+                        </List>
+                      </Collapse>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })}
           </List>
         )}
 
         {this.state.src === "" ? null : (
           //{/*<imgsrc={this.state.src}style={{ maxWidth: "100%", height: 500, marginTop: 30 }}alt={"Dog Pic"}/>*/}
-          <Card sx={{ maxWidth: 345, maxHeight: 300 , marginTop:10, position:'fixed', left:300}}>
+          <Card
+            sx={{
+              maxWidth: 345,
+              maxHeight: 300,
+              marginTop: 10,
+              position: "fixed",
+              right: 300,
+            }}
+          >
             <CardMedia
               component="img"
               height="200"
